4162473a2f5f3ea92a014da64782bbe65144e27a
Luke Yeager <luke.yeager@gmail.com>
2014-07-14 16:49:13 -0500

    Updated views to include server.updates

--- app/models/server.rb
+++ app/models/server.rb
@@ -3,4 +3,10 @@ class Server < ActiveRecord::Base
     validates :admin_password, presence: true
 
     has_many :updates
+
+    def recent_ip
+        u = updates.sort_by{|u| u.created_at}.last || nil
+        return nil if u.nil?
+        return u.ip_address
+    end
 end
--- app/views/servers/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @server, :id, :name, :description, :password, :admin_password, :created_at, :updated_at
